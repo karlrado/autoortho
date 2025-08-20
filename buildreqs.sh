10032d89da3b5469e7d247d64ebe83720a40a200
Andres Hernandez <andres.hernandezarvizu@mercadolibre.com.mx>
2025-08-20 12:28:47 -0600

    missing lib

--- buildreqs.sh
+++ buildreqs.sh
@@ -10,7 +10,9 @@ apt-get update
 #apt-get install -y make curl patchelf python3.10 python3.10-tk zlib1g-dev \
 #    ccache python3.10-distutils python3.10-dev libjpeg-dev libturbojpeg0-dev build-essential
 apt-get install -y make curl patchelf python3-pip python3-venv python3-tk zlib1g-dev \
-    ccache python3-setuptools python3-dev libjpeg-dev libturbojpeg0-dev build-essential
+    ccache python3-setuptools python3-dev libjpeg-dev libturbojpeg0-dev build-essential \
+    libglib2.0-0 libx11-6 libxcb1 libxkbcommon0 libdbus-1-3 libfontconfig1 libfreetype6 \
+    libgl1 libegl1
 
 # Create and prepare an isolated virtual environment to avoid PEP 668 restrictions
 python3 -m venv .venv
